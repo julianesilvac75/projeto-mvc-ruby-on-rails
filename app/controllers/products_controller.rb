3d2dac532e4c449cbc004b80a0df2e5fc553d3f0
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-21 20:29:57 -0300

    cria rota post /products e metodos que criam e salvam novos produtos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,9 +10,22 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_products
     @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :quantity, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
 
   get "products", to: "products#index"
   get "products/new", to: "products#new"
+  post "products", to: "products#create"
   get "products/:id", to: "products#show"
 end
