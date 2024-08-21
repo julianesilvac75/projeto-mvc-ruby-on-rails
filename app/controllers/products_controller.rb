a7276de172fcf0d16abf29e79d138acc00f47490
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-21 12:59:18 -0300

    refatorando controller de products, para que o metodo show possa ser utilizado em outros contextos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,14 @@
 class ProductsController < ApplicationController
+  before_action :set_products, only: [ :show ]
   def index
     @products = Product.all
   end
 
-  def show
+  def show; end
+
+  private
+
+  def set_products
     @product = Product.find(params[:id])
   end
 end
